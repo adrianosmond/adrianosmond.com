66b4ec7935fa4b82b2ea552ea62c6b065066aa54
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-01-24 10:32:32 +0100

    Meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'adrianosmond.com - The personal site of Adrian Osmond',
+    description: 'This is the personal site of front-end developer Adrian Osmond, where you can find out about me and my work.'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/templates/work.js
+++ src/templates/work.js
@@ -9,6 +9,14 @@ export default ({ data }) => {
     <article className="work-page">
       <Helmet title={`Work done with ${project.frontmatter.title} - Adrian Osmond`} />
       <div className="container">
+        {/* <img src={withPrefix(project.frontmatter.logo)} style={{
+          position: 'absolute',
+          zIndex: 0,
+          top: '5vh',
+          right: 0,
+          height: '50vh',
+          opacity: 0.05
+        }}/> */}
         <div className="content">
           <Link to="/" className="home-link">Back to the homepage</Link>
           <h1>{project.frontmatter.title}</h1>
