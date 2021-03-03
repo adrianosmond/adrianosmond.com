28c74b91c7f9d97444dc8cd6a02aaeb8759e5193
adrianosmond <adrian.osmond@ev-box.com>
2021-03-03 08:16:36 +0100

    Style tweaks

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,10 +4,10 @@ import './Nav.css';
 const Nav = () => (
   <nav className="nav">
     <a className="nav__link" href="#about-me">
-      About Me
+      About me
     </a>
     <a className="nav__link" href="#get-in-touch">
-      Get in Touch
+      Get in touch
     </a>
   </nav>
 );
